76a94150f01b35a14cc360aa615868dfe3abc0cc
Dave <dav3hit3@gmail.com>
2020-08-22 02:29:32 -0600

    Workiing on saving json data in form
    	modified:   static/report_parser/js/report_parser.js
    	modified:   templates/report_parser/userreport_form.html

--- report_parser/static/report_parser/js/report_parser.js
+++ report_parser/static/report_parser/js/report_parser.js
@@ -2,6 +2,7 @@
 function parseReport(report_data){
   console.log("parseReport( " + report_data + " )");
 
+  var json_data = null;
   
   require(['nearley', '/static/report_parser/js/grammar-base-compiled.js'], function (){
     console.log("now parsing (" + report_data + " )");
@@ -12,12 +13,14 @@ function parseReport(report_data){
 
     try{
       parser.feed(report_data);
-      console.log(parser.results);
+      //console.log(parser.results);
+      //json_data = parser.results;
     } catch(err){
       alert("Error parsing report!", err);
       console.log("Error parsing report: ", err);
     }
   });
 
+  return json_data;
 }
 
