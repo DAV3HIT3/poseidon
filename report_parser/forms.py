0e08470628bf4f1f5b08594e0e162cd8dc3c6a91
Dave <dav3hit3@gmail.com>
2020-08-22 01:37:35 -0600

    Add hidden field for report json_data
    modified:   forms.py

--- report_parser/forms.py
+++ report_parser/forms.py
@@ -5,6 +5,7 @@ from django import forms
 from .models import *
 
 class UserReportForm(ModelForm):
+    json_data = forms.CharField(widget=forms.HiddenInput())
     class Meta:
         model = UserReport
         fields = ["text",]
