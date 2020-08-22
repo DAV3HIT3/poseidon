6293f29f7b87a3fbc16d06425e116e66cd1f2cc3
Dave <dav3hit3@gmail.com>
2020-08-22 02:32:04 -0600

    modified:   report_parser.js

--- report_parser/static/report_parser/js/report_parser.js
+++ report_parser/static/report_parser/js/report_parser.js
@@ -1,20 +1,20 @@
 
 function parseReport(report_data){
-  console.log("parseReport( " + report_data + " )");
+  //console.log("parseReport( " + report_data + " )");
 
   var json_data = null;
   
   require(['nearley', '/static/report_parser/js/grammar-base-compiled.js'], function (){
-    console.log("now parsing (" + report_data + " )");
+    //console.log("now parsing (" + report_data + " )");
 
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(window.grammar));
 
-    console.log("In parseReport, calling parser(" + report_data + ")");
+    //console.log("In parseReport, calling parser(" + report_data + ")");
 
     try{
       parser.feed(report_data);
       //console.log(parser.results);
-      //json_data = parser.results;
+      json_data = parser.results;
     } catch(err){
       alert("Error parsing report!", err);
       console.log("Error parsing report: ", err);
