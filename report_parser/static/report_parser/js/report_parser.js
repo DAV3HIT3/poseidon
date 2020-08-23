2c4ab39ca8ea4b31e4dd8a20c8b5e3e359f03c23
Dave <dav3hit3@gmail.com>
2020-08-23 00:18:19 -0600

    Fixed saving json data from parsed report.
        modified:   static/report_parser/js/report_parser.js
        modified:   templates/report_parser/userreport_form.html

--- report_parser/static/report_parser/js/report_parser.js
+++ report_parser/static/report_parser/js/report_parser.js
@@ -1,23 +1,22 @@
 
 function parseReport(report_data){
-  //console.log("parseReport( " + report_data + " )");
-
   require(['nearley', '/static/report_parser/js/grammar-base-compiled.js'], function (){
-    //console.log("now parsing (" + report_data + " )");
 
+    // Create the parser with our compiled grammar
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(window.grammar));
 
-    //console.log("In parseReport, calling parser(" + report_data + ")");
-
     try{
+      // Feed the parser the report text
       parser.feed(report_data);
-      console.log(parser.results);
-      $('#id_json_data').val(parser.results);
-      return parser.results;
+      //console.log(parser.results[0]);
     } catch(err){
-      alert("Error parsing report!", err);
-      console.log("Error parsing report: ", err);
+      alert("Error parsing report! " + err);
+      console.log("Error parsing report: " + err);
     }
+    // Convert the parsed JSON into a string then add to the hidden json_data
+    // form field.
+    $('#id_json_data').text(JSON.stringify(parser.results[0]));
+    
   });
 }
 
