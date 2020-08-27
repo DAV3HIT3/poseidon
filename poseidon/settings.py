0fb28b782abb8c3808736737a9a1825cd7c9ff7b
Dave <dav3hit3@gmail.com>
2020-08-27 16:27:53 -0600

    Add sendgrid settings for sending e-mail
    modified:   poseidon/settings.py

--- poseidon/settings.py
+++ poseidon/settings.py
@@ -68,10 +68,13 @@ MIDDLEWARE = [
 
 # django-user-accounts settings
 SITE_ID = 1
-EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
-EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
-EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
-EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 ROOT_URLCONF = 'poseidon.urls'
 
